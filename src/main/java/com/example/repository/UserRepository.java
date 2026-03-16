935e043d554435408fcbdf97b542f33444ebfed1
takaoka <gr20f025@gmail.com>
2026-03-16 18:05:59 +0900

    初回コミット

--- src/main/java/com/example/repository/UserRepository.java
+++ src/main/java/com/example/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package com.example.repository;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
@@ -13,8 +12,8 @@ import com.example.domain.User;
 
 @Repository
 public class UserRepository {
-	
-	private static final RowMapper<User> USER_ROW_MAPPER =(rs,i)->{
+
+	private static final RowMapper<User> USER_ROW_MAPPER = (rs, i) -> {
 		User user = new User();
 		user.setId(rs.getInt("id"));
 		user.setName(rs.getString("name"));
@@ -27,28 +26,50 @@ public class UserRepository {
 
 	@Autowired
 	private NamedParameterJdbcTemplate template;
-	
-	public User  findByMailAddress(String email) {
-		String sql ="SELECT * FROM users WHERE email=:email";
-		
-		SqlParameterSource param = new MapSqlParameterSource().addValue("email",email);
-		
+
+	public User findByMailAddress(String email) {
+		String sql = "SELECT * FROM users WHERE email=:email";
+
+		SqlParameterSource param = new MapSqlParameterSource().addValue("email", email);
+
 		try {
-			User user= template.queryForObject(sql, param, USER_ROW_MAPPER);
+			User user = template.queryForObject(sql, param, USER_ROW_MAPPER);
 			System.out.println(user);
 			return user;
-		}catch(Exception e) {
+		} catch (Exception e) {
 			return null;
 		}
-		
+
 	}
-	
+
 	public void insert(User user) {
 		System.out.println(user);
 		SqlParameterSource param = new BeanPropertySqlParameterSource(user);
 		String sql = "INSERT INTO users (name, email, password, zipcode, address, telephone) "
-				+ "VALUES (:name, :email, :password, :zipcode, :address, :telephone);";	
-		template.update(sql, param);		
+				+ "VALUES (:name, :email, :password, :zipcode, :address, :telephone);";
+		template.update(sql, param);
 	}
-	
+
+	/**
+	 * ユーザー情報の更新
+	 * @param user
+	 */
+	public void update(User user){
+		SqlParameterSource param = new BeanPropertySqlParameterSource(user);
+		String sql = "UPDATE users SET name = :name, email = :email, zipcode = :zipcode, address = :address, telephone = :telephone";
+		template.update(sql, param);
+	}
+
+	/**
+	 * 論理削除
+	 * @param id
+	 */
+	public void delete(Integer id) {
+		SqlParameterSource param = new MapSqlParameterSource()
+				.addValue("id", id)
+				.addValue("status", 1);
+		String sql = "UPDATE users SET status = :status WHERE id = :id";
+		template.update(sql, param);
+	}
+
 }
