c8f7e22b66783af9e32d0aae614dc03980e506ee
masashi.saito <masashi.saito@rakus-partners.co.jp>
2026-03-17 11:05:53 +0900

    スキーマ変更に伴うdomainクラスの修正 #100

---
+++ src/main/java/com/example/domain/StampHistory.java
@@ -0,0 +1,58 @@
+package com.example.domain;
+
+/**
+ * stamp_historyテーブルのdomainクラス
+ *
+ * @author masashi.saito
+ */
+public class StampHistory {
+
+    /**
+     * id
+     */
+    private Integer id;
+
+    /**
+     * ユーザid
+     */
+    private Integer userId;
+
+    /**
+     * 注文id
+     */
+    private Integer orderId;
+
+    /**
+     * 注文idのなかのスタンプ増減数
+     */
+    private Integer stampCountChenges;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Integer orderId) {
+        this.orderId = orderId;
+    }
+
+    public Integer getStampCountChenges() {
+        return stampCountChenges;
+    }
+
+}
--- src/main/java/com/example/domain/User.java
+++ src/main/java/com/example/domain/User.java
@@ -1,29 +1,39 @@
 package com.example.domain;
+
 /**
  * 
  * @author satakemisako
- * ユーザー情報のドメインクラス
+ *         ユーザー情報のドメインクラス
  *
  */
 public class User {
 
-	//ID
+	// ID
 	private Integer id;
-	//ユーザー氏名
+	// ユーザー氏名
 	private String name;
-	//パスワード
+	// パスワード
 	private String password;
-	//メールアドレス
+	// メールアドレス
 	private String email;
-	//郵便番号
+	// 郵便番号
 	private String zipcode;
-	//住所
+	// 住所
 	private String address;
-	//電話番号
+	// 電話番号
 	private String telephone;
-	
-	
-	//引数ありコンストラクタ
+
+	/**
+	 * 現在のスタンプ数
+	 */
+	private Integer stampNowCount;
+
+	/**
+	 * 今まで貯めたスタンプの総数
+	 */
+	private Integer stampAllCount;
+
+	// 引数ありコンストラクタ
 	public User(Integer id, String name, String password, String email, String zipcode, String address,
 			String telephone) {
 		super();
@@ -35,59 +45,89 @@ public class User {
 		this.address = address;
 		this.telephone = telephone;
 	}
-	//引数なしコンストラクタ
-	public User() {}
-	
-	//以下getter及びsetter
+
+	// 引数なしコンストラクタ
+	public User() {
+	}
+
+	// 以下getter及びsetter
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public String getZipcode() {
 		return zipcode;
 	}
+
 	public void setZipcode(String zipcode) {
 		this.zipcode = zipcode;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
+
 	public String getTelephone() {
 		return telephone;
 	}
+
 	public void setTelephone(String telephone) {
 		this.telephone = telephone;
 	}
-	//toStringメソッド
+
+	public Integer getStampNowCount() {
+		return stampNowCount;
+	}
+
+	public void setStampNowCount(Integer stampNowCount) {
+		this.stampNowCount = stampNowCount;
+	}
+
+	public Integer getStampAllCount() {
+		return stampAllCount;
+	}
+
+	public void setStampAllCount(Integer stampAllCount) {
+		this.stampAllCount = stampAllCount;
+	}
+
 	@Override
 	public String toString() {
 		return "User [id=" + id + ", name=" + name + ", password=" + password + ", email=" + email + ", zipcode="
-				+ zipcode + ", address=" + address + ", telephone=" + telephone + "]";
+				+ zipcode + ", address=" + address + ", telephone=" + telephone + ", stampNowCount=" + stampNowCount
+				+ ", stampAllCount=" + stampAllCount + "]";
 	}
-	
-	
-	
+
 }
