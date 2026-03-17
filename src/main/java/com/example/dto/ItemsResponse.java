2bde61faf910c6dfc991c085a50b0233d3453f42
rentsuchiya <ren.tsuchiya@rakus-partners.co.jp>
2026-03-17 15:57:40 +0900

    Update ItemsResponse.java

--- src/main/java/com/example/dto/ItemsResponse.java
+++ src/main/java/com/example/dto/ItemsResponse.java
@@ -11,11 +11,7 @@ public class ItemsResponse {
     private Integer priceM;
     /**Lサイズの値段 */
     private Integer priceL;
-<<<<<<< HEAD
     /**商品画像*/
-=======
-    /**商品画像 */
->>>>>>> origin/develop
     private String imagePath;
     public Integer getId() {
         return id;
