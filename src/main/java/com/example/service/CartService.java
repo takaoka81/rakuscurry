cb7f980776a95eff560b3250f5f3ebd8d0e10c82
takaoka <gr20f025@gmail.com>
2026-08-18 15:14:45 +0900

    DBエラー修正

--- src/main/java/com/example/service/CartService.java
+++ src/main/java/com/example/service/CartService.java
@@ -10,7 +10,6 @@ import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
